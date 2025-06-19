f6b0e38e440d242ba5cda2c2162e313fa74789c9
inspiredlabs <alan.scott.phillips@protonmail.ch>
2025-06-19 14:26:22 +0200

    Fixed the margin overflow issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,55 +66,72 @@
   <!-- `items` are WORKING: <pre>{JSON.stringify(items, null, 2)}</pre> -->
   
   <section class="z-1 relative ml-auto mr-auto">
-  {#if isLoading}
-    <p>Loading feeds, please wait...</p>
-  {:else}
-    <ul class="list pl0 measure-wide">
-      <!-- Now loop over `filteredItems` and display results -->
-      {#each filteredItems as {
-        title, link, author,
-        pubDate, description,
-        content, feedName, feedColor
-      }}
-        <li class="pa3 br4 mb3 bg-white overflow-hidden" >
-          <a href="{link}" class="link" style="color: #383838">
-            <h2 class="dark-gray">{@html title}</h2>
-            <h3 class="gray">{@html description}</h3>
-            <!--p>{@html content}</p-->
-          </a>
-          <small class="b">
-            <cite style="color: {feedColor}">{@html feedName}<!--span style="display: {author ? 'inline-block' : 'none'}">: {@html author}</span--></cite>
-            <date>{new Date(pubDate).toLocaleString()}</date>	
-          </small>
-        </li>
-      {:else}
-        <!-- Show a message if the filter returns no results -->
-        <li>No items match your search.</li>
-      {/each}
-    </ul>
-  {/if}
-  </section>
+    {#if isLoading}
+      <p>Loading feeds, please wait...</p>
+    {:else}
+      <ul class="list pa3 measure-wide">
+        <!-- Now loop over `filteredItems` and display results -->
+        {#each filteredItems as {
+          title, link, author,
+          pubDate, description,
+          content, feedName, feedColor
+        }}
+          <li class="pa3 br4 mb3 bg-white overflow-hidden" >
+            <a href="{link}" class="link" style="color: #383838">
+              <h2 class="dark-gray">{@html title}</h2>
+              <h3 class="gray">{@html description}</h3>
+              <!--p>{@html content}</p-->
+            </a>
+            <small class="b">
+              <cite style="color: {feedColor}">{@html feedName}<!--span style="display: {author ? 'inline-block' : 'none'}">: {@html author}</span--></cite>
+              <date>{new Date(pubDate).toLocaleString()}</date>	
+            </small>
+          </li>
+        {:else}
+          <!-- Show a message if the filter returns no results -->
+          <li>No items match your search.</li>
+        {/each}
+      </ul>
+    {/if}
+    </section>
   </main>
-  
+
 <style>
-.w-25 { width: 25% }
-.ml-auto { margin-left: auto }
-.mr-auto { margin-right: auto }	
+:global(body) {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
+  background-color: #f8f8f8;
+  width: 100%;
+  margin: 0;
+}
 .sticky {	
   position: sticky;
   top: 0;
 }
+.w-25 { width: 25% }
+.flex { display: flex }
+.flex-column { flex-direction: column }
 .bg-gray { background-color: #777 }	
 .z-1 {z-index: 1}
 .z-2 {z-index: 2}	
 .relative { position: relative }
-.list { list-style-type: none }
-.pl0 { padding-left: 0 }
+.w-100  { width: 100% }
+.pa0 { padding: 0 }
 .pa3 { padding: 1rem }
+.bg-white { background-color: #fff }
 .br4 { border-radius: 1rem }
 .mb3 { margin-bottom: 1rem }
-.bg-white { background-color: #fff }
-.overflow-hidden { overflow: hidden }
+ul { box-sizing: border-box }
+.list { list-style-type: none }
+
+li {
+  max-width: 100%;
+  overflow-wrap: break-word;
+  box-sizing: border-box;
+}
+
+.measure-wide { max-width: 34em }
+
 .link { text-decoration: none }
 .link,
 .link:active,
@@ -127,32 +144,29 @@
 .link:focus {
   outline: 1px dotted currentColor;
 }
+
+
+
+
+/* THIS IS THE OVERFLOW PROBLEM:
+.ml-auto { margin-left: auto }
+.mr-auto { margin-right: auto } */
+</style>
+
+<!-- <style>
+
+
+
+.overflow-hidden { overflow: hidden }
+
 .list {
   list-style-type: none;
 }
+
 .w-100  { width: 100% }
-.measure-wide { max-width: 34em }
-.flex { display: flex }
-.flex-column { flex-direction: column }
+
 .justify-around { justify-content: space-around }	
 .dark-gray { color: #333 }
 .gray { color: #777 }
 .b { font-weight: bold }
-  
-:global(body) {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-    'Open Sans', 'Helvetica Neue', sans-serif;
-  background-color: #f8f8f8;
-  margin: 0;
-}
-
-:global( .sr-only:not(:focus):not(:active) ) {
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  height: 1px;
-  overflow: hidden;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
-}
-</style>
+</style> -->
